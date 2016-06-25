863b7c8cd3e748ff98d1899708f5c29e070f9fee
aishu0902 <aish0292@gmail.com>
2016-06-25 15:30:05 +0000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+<html>
+<head>
+</head>
+<body>
+<scipt>
+var course= 2;
+if (course == 1)
+{
+document.write("Tableau");
+}
+else if (course == 2)
+{
+document.write("Servicenow");
+}
+else
+{
+document.write("Java");
+}
+
+</script>
+</body>
+</html>
